d628dd671bf605cf354ed1294bc874550186594f
jugulmishra2007 <jugulmishra2007@gmail.com>
2018-02-07 15:10:26 +0530

    updating in github

---
+++ src/main/java/com/test/springboot/configuration/JpaConfiguration.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.test.springboot.configuration;
+
+import java.util.Properties;
+
+/**
+ * @author jugul.mishra 
+ * Feb 7, 2018 9:41:27 AM
+ *
+ */
+public interface JpaConfiguration {
+	public Properties setup();
+}
